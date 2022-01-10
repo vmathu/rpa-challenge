d87af6ede03503754b6676267f72f1f71d6d5278
Trần Anh Quân <57850219+quanchan@users.noreply.github.com>
2022-01-10 11:50:37 +0700

    Update agencies_list_parser.py

--- agencies_list_parser.py
+++ agencies_list_parser.py
@@ -1,10 +1,13 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
-
+# Critical: Don't import Excel everywhere. It defeat the purpose of using class and encapsulation. You should create an Excel writer (with methods for 
+# writing different files) and then import that class in here.  
 
 class AgenciesListParser:
     URL = "https://itdashboard.gov"
 
+    # Medium: Please move this into where is it use, it is only used once. It is bad to have global variable like this. And the name of this variable 
+    # is not descriptive. Don't use generic words for variable name.
     array = []
 
     def __init__(self):
@@ -26,6 +29,8 @@ class AgenciesListParser:
             '//*[@id="agency-tiles-container"]'
         )
         return container.find_elements_by_class_name('noUnderline')
+    # Critical: Never, ever use styling classname to find an element. Styling can change. However structure will not. Please use structure like the example from 
+    # https://github.com/kvazarich/IT_Dashboard_RPA_Challenge
 
     def _get_name(self, agency):
         self.browser.wait_until_element_is_visible(
@@ -42,9 +47,11 @@ class AgenciesListParser:
         return self.browser.get_webelement(
             locator=[agency, 'css:span:nth-of-type(2)']
         )
+# Medium: Your naming convention is really inconsistent, please unify them by whether or not to put a _ at the start of methods names
 
     def get_information(self):
         agencies = self.get_agencies()
+        # Critical: Move this entire writing method into an ExcelWriter class
         self.excel.create_workbook("output/Agencies.xlsx")
         self.excel.rename_worksheet("Sheet", "Agencies")
         row=1
@@ -60,4 +67,6 @@ class AgenciesListParser:
     
     def get_agency_link(self, id):
         agency = self.get_agencies()[id]
+    # Critical: Never, ever use styling classname to find an element. Styling can change. However structure will not. Please use structure like the example from 
+    # https://github.com/kvazarich/IT_Dashboard_RPA_Challenge
         return agency.find_element_by_class_name('btn-sm').get_attribute('href')
